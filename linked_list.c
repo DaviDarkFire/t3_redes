497b37c34e2a4120a4bc2d04d63d6f5a3fd44f48
caiosuzuki <cpolidoro@gmail.com>
2018-10-25 23:01:25 -0400

    linked list funciona com endereços ethernet decentemente, falta tratar pra printar endereços ip; funçao de busca de nó pelo ip address está feita e testada

--- linked_list.c
+++ linked_list.c
@@ -7,12 +7,12 @@
 
 typedef struct node{
   int ip_address;
-  char eth_address[6];
+  unsigned char eth_address[6];
   int ttl;
   struct node* next;
 } node_t;
 
-node_t* add_node(node_t* current_last_node, int ip_address, char eth_address[6], int ttl){
+node_t* add_node(node_t* current_last_node, int ip_address, unsigned char eth_address[6], int ttl){
     node_t* new_node;
     new_node = (node_t*) malloc(sizeof(node_t));
     new_node->ip_address = ip_address;
@@ -60,7 +60,7 @@ int delete_node_by_ip_address(node_t** head, int del_ip_addr){
 void print_list(node_t * head){
   node_t * current = head;
   int i;
-  printf("Entrada\tEndereço IP\t\t\tEndereço Ethernet\t\tTTL\n");
+  printf("Entrada\t\tEndereço IP\t\tEndereço Ethernet\t\tTTL\n");
   for(i = 0; current != NULL; i++){
     // TODO: printar direito os endereços
     printf("%d\t\t%d\t\t\t%02X:%02X:%02X:%02X:%02X:%02X\t\t%d\n",
@@ -71,32 +71,60 @@ void print_list(node_t * head){
   }
 }
 
+// modifies dest_array with 6 separate bytes on each array element given a string with format XX:XX:XX:XX:XX:XX
+void get_eth_addr_as_6_bytes_from_string(unsigned char dest_array[6], char* src_str){
+    int i_aux[6];
+
+    sscanf(src_str, "%x:%x:%x:%x:%x:%x",
+    &i_aux[0], &i_aux[1], &i_aux[2],
+    &i_aux[3], &i_aux[4], &i_aux[5]);
+
+    dest_array[0] = (unsigned char) i_aux[0];
+    dest_array[1] = (unsigned char) i_aux[1];
+    dest_array[2] = (unsigned char) i_aux[2];
+    dest_array[3] = (unsigned char) i_aux[3];
+    dest_array[4] = (unsigned char) i_aux[4];
+    dest_array[5] = (unsigned char) i_aux[5];
+}
+
+// returns desired node if it exists in the list, otherwise, returns NULL (be careful!)
+node_t* find_node_by_ip_address(node_t* head, int desired_ip_addr){
+    node_t* current = head;
+    while(current != NULL){
+      if(current->ip_address == desired_ip_addr)
+        return current;
+      current = current->next;
+    }
+    return NULL;
+}
+
 int main(int argc, char** argv){
-    int eth_addr[6];
+    unsigned char c_eth_addr[6];
 
-    sscanf(argv[1], "%x:%x:%x:%x:%x:%x",
-    &eth_addr[0], &eth_addr[1], &eth_addr[2],
-    &eth_addr[3], &eth_addr[4], &eth_addr[5]);
+    get_eth_addr_as_6_bytes_from_string(c_eth_addr ,argv[1]);
 
-    printf("%02X:%02X:%02X:%02X:%02X:%02X\n",
-    eth_addr[0], eth_addr[1],
-    eth_addr[2], eth_addr[3],
-    eth_addr[4], eth_addr[5]);
+    // printf("char: %02X:%02X:%02X:%02X:%02X:%02X\n",
+    // c_eth_addr[0], c_eth_addr[1],
+    // c_eth_addr[2], c_eth_addr[3],
+    // c_eth_addr[4], c_eth_addr[5]); // DEBUG
 
-    node_t* first_node = add_node(NULL, 0, "000000", 0);
+    node_t* first_node = add_node(NULL, 0, c_eth_addr, 0);
     node_t* aux = first_node;
     node_t* node;
-    char* string_teste = "123456";
-    // strcpy(string_teste,"123456");
-    // printf("string_teste: %d %d %d %d %d %d\n", string_teste[0], string_teste[1],
-    //     string_teste[2], string_teste[3], string_teste[4], string_teste[5]);
 
-    // printf("string_test: %s\n", string_teste);
     for (int i = 1; i < 10; i++){
-        node = add_node(first_node, i, string_teste, i);
+        node = add_node(first_node, i, c_eth_addr, i);
         first_node = node;
     }
-    delete_node_by_ip_address(&aux,9);
+    node_t* find_ip_5 = find_node_by_ip_address(aux, 12);
+    if(find_ip_5 != NULL){
+        printf("Achei o nó com ip: %d\n", find_ip_5->ip_address);
+    } else {
+      printf("achei nulo\n");
+    }
+
+
+    // delete_node_by_ip_address(&aux,0); // DEBUG
     print_list(aux);
 
 }
