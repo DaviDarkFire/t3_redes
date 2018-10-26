e8b390e2f5ad9094a4ef443fd44a15079f4077a8
davi <daviferreirasantog@gmail.com>
2018-10-25 21:32:50 -0400

    linked list quase finalizada

--- linked_list.c
+++ linked_list.c
@@ -16,9 +16,10 @@ node_t* add_node(node_t* current_last_node, int ip_address, char eth_address[6],
     node_t* new_node;
     new_node = (node_t*) malloc(sizeof(node_t));
     new_node->ip_address = ip_address;
-    printf("eth_address dentro da addnode: %d %d %d %d %d %d\n", eth_address[0], eth_address[1],
-        eth_address[2], eth_address[3], eth_address[4], eth_address[5]);
-    // strncpy(new_node->eth_address, eth_address, 6);
+    // printf("TAMANHO eth_address: %s\n", eth_address);
+    // printf("eth_address dentro da addnode: %c %c %c %c %c %c\n", eth_address[0], eth_address[1],
+    //     eth_address[2], eth_address[3], eth_address[4], eth_address[5]);
+    strncpy(new_node->eth_address, eth_address, 6);
     new_node->ttl = ttl;
     new_node->next = NULL;
     if(current_last_node != NULL)
@@ -32,8 +33,17 @@ int delete_node_by_ip_address(node_t** head, int del_ip_addr){
   node_t* current = *head;
   node_t* previous = NULL;
 
+  //exclusão da head
+  if(del_ip_addr == (*head)->ip_address){
+    retval = del_ip_addr;
+    node_t* aux = *head;
+    *head = (*head)->next;
+    free(aux);
+    return retval;
+  }
+
   while(1){
-    if(current->next == NULL)
+    if(current == NULL)
       return -1;
     if(current->ip_address == del_ip_addr) // current is the node we'll delete
       break;
@@ -61,32 +71,32 @@ void print_list(node_t * head){
   }
 }
 
-int main(void){
-  // char eth_addr[6];
-  // int ip_addr;
-  // struct in_addr aux_ip_addr;
-  // char* ip_string;
-  // int list_len = 0;
-  // // TODO: ler o eth_address corretamente
-  // // sscanf("a2:b6:67:e4:aa:87", "%x:%x:%x:%x:%x:%x",
-  // //  &eth_addr[0], &eth_addr[1], &eth_addr[2],
-  // //   &eth_addr[3], &eth_addr[4], &eth_addr[5]);
-  // scanf("%s", ip_string);
-  // if(inet_aton(ip_string ,&aux_ip_addr) == 0){
-  //   fprintf(stderr, "Invalid address\n");
-  //   exit(0);
-  // }
-  // ip_addr = (int) aux_ip_addr.s_addr;
-    node_t* first_node = add_node(NULL, 0, (char*) 0, 0);
+int main(int argc, char** argv){
+    int eth_addr[6];
+
+    sscanf(argv[1], "%x:%x:%x:%x:%x:%x",
+    &eth_addr[0], &eth_addr[1], &eth_addr[2],
+    &eth_addr[3], &eth_addr[4], &eth_addr[5]);
+
+    printf("%02X:%02X:%02X:%02X:%02X:%02X\n",
+    eth_addr[0], eth_addr[1],
+    eth_addr[2], eth_addr[3],
+    eth_addr[4], eth_addr[5]);
+
+    node_t* first_node = add_node(NULL, 0, "000000", 0);
     node_t* aux = first_node;
     node_t* node;
-    char string_teste[6] = {1, 2, 3, 4, 5, 6};
-    printf("string_teste: %d %d %d %d %d %d\n", string_teste[0], string_teste[1],
-        string_teste[2], string_teste[3], string_teste[4], string_teste[5]);
+    char* string_teste = "123456";
+    // strcpy(string_teste,"123456");
+    // printf("string_teste: %d %d %d %d %d %d\n", string_teste[0], string_teste[1],
+    //     string_teste[2], string_teste[3], string_teste[4], string_teste[5]);
+
+    // printf("string_test: %s\n", string_teste);
     for (int i = 1; i < 10; i++){
         node = add_node(first_node, i, string_teste, i);
         first_node = node;
     }
+    delete_node_by_ip_address(&aux,9);
     print_list(aux);
 
 }
