933061971762e334989565d3c8295466aeeaeb61
caiosuzuki <cpolidoro@gmail.com>
2018-10-28 17:45:01 -0400

    limpando código da xifconfig, tirando ip_config comentada e fazendo set_mtu ser mais concisa

--- xifconfig/xifconfig.c
+++ xifconfig/xifconfig.c
@@ -13,46 +13,14 @@ void set_mtu_mode(char* iface, char* new_mtu){
   struct ifreq ifr;
 
   strcpy(ifr.ifr_name, iface);
-  if(!ioctl(sockfd, SIOCGIFMTU, &ifr)) {
-    // ifr.ifr_mtu // Contains current mtu value
-  }
+
   ifr.ifr_mtu = atoi(new_mtu);
   if(!ioctl(sockfd, SIOCSIFMTU, &ifr)) {
     printf("MTU changed successfully.\n");
   }
 }
 
-// based on ifconfig source code
-// void configure_ip_mode(char* iface, char* ip_addr){
-//   struct ifreq ifr;
-//   struct sockaddr_in sai;
-//   int sockfd;
-//   char *p;
-//
-//   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-//
-//   // setting interface name
-//   strncpy(ifr.ifr_name, iface, IFNAMSIZ); // IFNAMSIZ is the maximum iface name length
-//
-//   memset(&sai, 0, sizeof(struct sockaddr));
-//   sai.sin_family = AF_INET;
-//   sai.sin_port = 0;
-//
-//   sai.sin_addr.s_addr = inet_addr(ip_addr);
-//
-//   p = (char *) &sai;
-//   memcpy( (((char *)&ifr + ifreq_offsetof(ifr_addr) )), p, sizeof(struct sockaddr));
-//
-//   ioctl(sockfd, SIOCSIFADDR, &ifr);
-//   ioctl(sockfd, SIOCGIFFLAGS, &ifr);
-//
-//   ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
-//
-//   ioctl(sockfd, SIOCSIFFLAGS, &ifr);
-//   close(sockfd);
-// }
-
-void configure_ip_mode(char* iface, char* ip_addr){
+void configure_ip_mode(char* iface, char* ip_addr, char* netmask){
     struct ifreq ifr;
     int sockfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
 
@@ -61,7 +29,7 @@ void configure_ip_mode(char* iface, char* ip_addr){
     inet_pton(AF_INET, ip_addr, ifr.ifr_addr.sa_data + 2);
     ioctl(sockfd, SIOCSIFADDR, &ifr);
 
-    inet_pton(AF_INET, "255.255.0.0", ifr.ifr_addr.sa_data + 2);
+    inet_pton(AF_INET, netmask, ifr.ifr_addr.sa_data + 2);
     ioctl(sockfd, SIOCSIFNETMASK, &ifr);
 
     ioctl(sockfd, SIOCGIFFLAGS, &ifr);
@@ -80,7 +48,7 @@ int main(int argc, char** argv){
 
     case CONFIG_IP_MODE:
       printf("You chose ip configuration mode.\n"); // DEBUG
-      configure_ip_mode(argv[1], argv[2]);
+      configure_ip_mode(argv[1], argv[2], argv[3]);
       printf("Interface's IP is configured as desired.\n"); // DEBUG
     break;
 
