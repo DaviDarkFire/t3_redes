ff46c4284421a90fe66fc6841bb28f60c50f8d4c
caiosuzuki <cpolidoro@gmail.com>
2018-11-01 23:13:18 -0400

    build_xarp_add_message feita e compilando, nao testada pois ainda nao tem communication

--- xarp/xarp.c
+++ xarp/xarp.c
@@ -1,5 +1,6 @@
 #include "xarp.h"
 
+// xarp show
 char* build_xarp_show_message(){
   char* message;
   message = malloc(1);
@@ -7,16 +8,43 @@ char* build_xarp_show_message(){
   return message;
 }
 
+// xarp res EndereçoIP
 char* build_xarp_res_message(char** args){
   char* message;
-  message = malloc(5);
-  strcpy(message, (char*)XARP_DEL);
+  message = malloc(5); // 1 + 4
+  strcpy(message, (char*)XARP_RES);
   strcat(message, args[2]); // sends the desired ip address along with the opcode
   return message;
 }
 
+char* get_mac_addr_bytes_from_string(char* colon_format_mac){
+  char* mac_bytes;
+  int i;
+  unsigned int i_aux[6];
+  mac_bytes = malloc(sizeof(char) * 6);
+  sscanf(colon_format_mac, "%X:%X:%X:%X:%X:%X",
+	&i_aux[0], &i_aux[1], &i_aux[2],
+	&i_aux[3], &i_aux[4], &i_aux[5]);
+
+  for(i = 0; i < 6; i++){
+    mac_bytes[i] = (char) i_aux[i];
+  }
+
+  return mac_bytes;
+}
+
+//xarp add EndereçoIP EndereçoEthernet ttl
 char* build_xarp_add_message(char** args){
-  
+  char* mac_bytes;
+  char* message;
+  message = malloc(15); // 1 + 4 + 6 + 4
+  mac_bytes = get_mac_addr_bytes_from_string(args[3]);
+  strcpy(message, (char*)XARP_ADD);
+  strcat(message, args[2]);
+  strcat(message, mac_bytes);
+  strcat(message, args[4]);
+  free(mac_bytes);
+  return message;
 }
 
 int main(int argc, char** argv){
@@ -32,15 +60,15 @@ int main(int argc, char** argv){
     }
     else{
       if(strcmp(op, "add") == 0){
-        // message = build_xarp_add_message(argv);
+        message = build_xarp_add_message(argv);
       }
       else{
         if(strcmp(op, "del") == 0){
-          // message = build_xarp_add_message(args);
+          // message = build_xarp_del_message(args);
         }
         else{
           if(strcmp(op, "ttl") == 0){
-            // message = build_xarp_res_message();
+            // message = build_xarp_ttl_message();
           }
         }
       }
